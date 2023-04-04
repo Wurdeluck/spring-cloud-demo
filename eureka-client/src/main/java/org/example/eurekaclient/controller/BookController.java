bec27b425992787cfa6e64e0371dbf05403bec55
Andrei Repkin <repkin572@gmail.com>
2023-04-04 16:46:43 +0200

    - Added multiple instances support

--- eureka-client/src/main/java/org/example/eurekaclient/controller/BookController.java
+++ eureka-client/src/main/java/org/example/eurekaclient/controller/BookController.java
@@ -1,5 +1,6 @@
 package org.example.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/books")
 public class BookController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @GetMapping("/{bookId}")
     public String book(@PathVariable String bookId) {
         return "Ama book numba " + bookId;
+    }
+
+    @GetMapping("/about-instance")
+    public String aboutInstance() {
+        return "Ama instance namba " + instanceId;
     }
 }
