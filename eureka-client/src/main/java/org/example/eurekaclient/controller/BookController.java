1f6eee618f3abee6163bb743107123629301f4f6
Andrei Repkin <repkin572@gmail.com>
2023-04-04 15:10:30 +0200

    - Added API Gateway - Added custom route for microservice client

--- eureka-client/src/main/java/org/example/eurekaclient/controller/BookController.java
+++ eureka-client/src/main/java/org/example/eurekaclient/controller/BookController.java
@@ -1,12 +1,15 @@
 package org.example.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1/books")
 public class BookController {
-    @GetMapping("/book")
-    public String book() {
-        return "Ama book";
+    @GetMapping("/{bookId}")
+    public String book(@PathVariable String bookId) {
+        return "Ama book numba " + bookId;
     }
 }
